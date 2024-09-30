de1c060c74e46dd2647ff8dd5b9227547b18d513
bmuszynska <beata.muszynska.it@gmail.com>
2024-09-30 22:10:13 +0200

    Voters controlles
    Add CRUD operations for voters

--- api/Controllers/VotersController.cs
+++ api/Controllers/VotersController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
-    public class VotersController : Controller
+    public class VotersController : BaseApiController
     {
-        public IActionResult Index()
+        private readonly VoteAppContext _context;
+
+        public VotersController(VoteAppContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Voter>>> GetVoters()
+        {
+            return await _context.Voters.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Voter>> GetVoter(int id)
+        {
+            var voter = await _context.Voters.FindAsync(id);
+            if (voter == null)
+            {
+                return NotFound();
+            }
+
+            return voter;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Voter>> AddNewVoter(Voter voter)
+        {
+            _context.Voters.Add(voter);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetVoter), new { id = voter.Id }, voter);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Voter>> VoterHasVoted(int id, Voter voter)
+        {
+            if (id != voter.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(voter).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            return Ok(voter);
         }
     }
 }
