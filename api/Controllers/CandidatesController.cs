78e11894de4cdcc19059fd87969680afe0c6a6b8
bmuszynska <beata.muszynska.it@gmail.com>
2024-09-30 22:11:09 +0200

    Candidates Controller
    Add CRUD operations for candidates

--- api/Controllers/CandidatesController.cs
+++ api/Controllers/CandidatesController.cs
@@ -1,12 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
-    public class CandidatesController : Controller
+    public class CandidatesController : BaseApiController
     {
-        public IActionResult Index()
+        private readonly VoteAppContext _context;
+
+        public CandidatesController(VoteAppContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Candidate>>> GetCandidates()
+        {
+            return await _context.Candidates.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Candidate>> GetCandidate(int id)
+        {
+            var candidate = await _context.Candidates.FindAsync(id);
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            return candidate;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Candidate>> AddNewCandidate(Candidate candidate)
+        {
+            _context.Candidates.Add(candidate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCandidate), new { id = candidate.Id }, candidate);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Candidate>> CandidateGotVote(int id, Candidate candidate)
+        {
+            if (id != candidate.Id)
+            {
+                return BadRequest();
+            }
+
+            var candidateOriginal = _context.Candidates.FindAsync(id);
+            candidateOriginal.Result.VoteCount++;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            return Ok(candidate);
         }
     }
 }
