3481954f525017b5559546c59ad8a243f7cc5819
bmuszynska <beata.muszynska.it@gmail.com>
2024-10-01 22:52:51 +0200

    Error handling in CandidatesController
    Added error/exception handling in CandidatesController

--- api/Controllers/CandidatesController.cs
+++ api/Controllers/CandidatesController.cs
@@ -16,34 +16,67 @@ namespace api.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Candidate>>> GetCandidates()
         {
-            return await _context.Candidates.ToListAsync();
+            try
+            {
+                return await _context.Candidates.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message, stackTrace = ex.StackTrace });
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Candidate>> GetCandidate(int id)
         {
             var candidate = await _context.Candidates.FindAsync(id);
+
             if (candidate == null)
             {
                 return NotFound();
             }
 
-            return candidate;
+            return Ok(candidate);
         }
 
         [HttpPost]
         public async Task<ActionResult<Candidate>> AddNewCandidate(Candidate candidate)
         {
-            _context.Candidates.Add(candidate);
-            await _context.SaveChangesAsync();
+            if (candidate.Id != 0)
+            {
+                var existingVoter = await GetCandidate(candidate.Id);
+                if (existingVoter.Result is not NotFoundResult)
+                {
+                    return Conflict("A voter with the same ID already exists.");
+                }
+            }
 
-            return CreatedAtAction(nameof(GetCandidate), new { id = candidate.Id }, candidate);
+            _context.Candidates.Add(candidate);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "A concurrency error occurred while saving changes to the database.");
+            }
+            catch (DbUpdateException dbEx)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while saving changes to the database.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message, stackTrace = ex.StackTrace });
+            }
+
+            return CreatedAtAction(nameof(GetCandidate), new { id = candidate.Id }, candidate); ;
         }
 
         [HttpPatch("{id}/gotVote")]
         public async Task<ActionResult<Candidate>> GotVote(int id)
         {
-            var candidate = _context.Candidates.FindAsync(id).Result;
+            var candidate = await _context.Candidates.FindAsync(id);
+
             if (candidate == null)
             {
                 return NotFound();
@@ -51,9 +84,7 @@ namespace api.Controllers
 
             candidate.VoteCount++;
 
-            await _context.SaveChangesAsync();
-
-            return Ok(candidate);
+            return SaveChanges(candidate).Result;
         }
     }
 }
