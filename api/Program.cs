48bb812eee152a93328d0bc56cee34a388b349e2
bmuszynska <beata.muszynska.it@gmail.com>
2024-10-02 10:22:26 +0200

    Update Program.cs
    Add exception handler and https

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,4 @@
+using api.Exceptions;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddCors();
 builder.Services.AddScoped<IVoteAppContext, VoteAppContext>();
-
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<VoteAppContext>(opt =>
 {
@@ -17,16 +19,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseExceptionHandler();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
-app.UseCors(c => c.WithOrigins("http://localhost:4200")
+app.UseCors(c => c.WithOrigins("https://localhost:4200")
             .AllowAnyHeader()
             .AllowAnyMethod());
 
